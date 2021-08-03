b6b2898156075cd2dd6b2a7906713a51d4f01dfa
Kory Attleson <kory.attleson@gmail.com>
2021-08-03 14:53:54 -0500

    created new .js file

---
+++ algorithmOne.js
@@ -0,0 +1,8 @@
+// Given an array of numbers, return indices of two numbers that would 
+// add up to a specific target. 
+
+let numbers = [5, 17, 77, 50]
+
+function randomNumbers(number){
+    console.log(numbers[Math.floor(Math.random() *numbers.length)]);
+}
